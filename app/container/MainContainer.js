bd5f46052f647e4c8cda532c90644290f503c9f8
Fahim Sakil Khan <fahimskhan@Fahims-MacBook-Air.local>
2018-08-27 14:40:20 -0700

    bold, italcize and underline implemented

--- app/container/MainContainer.js
+++ app/container/MainContainer.js
@@ -1,23 +1,42 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Editor } from 'draft-js';
+import { Editor, EditorState, RichUtils } from 'draft-js';
 import { onChange } from '../actions/index';
 
-import { EditorState } from 'draft-js';
-
-
 class MainContainer extends React.Component {
+  _onBoldClick() {
+    this.props.onChange(RichUtils.toggleInlineStyle(
+      this.props.editorState,
+      'BOLD'
+    ));
+  }
+
+  _onItalicClick() {
+    this.props.onChange(RichUtils.toggleInlineStyle(
+      this.props.editorState,
+      'ITALIC'
+    ));
+  }
+
+  _onUnderlineClick() {
+    this.props.onChange(RichUtils.toggleInlineStyle(
+      this.props.editorState,
+      'UNDERLINE'
+    ));
+  }
 
   render() {
     return (
       <div>
-        <h4>Editor</h4>
         <div className="editor">
-        <Editor
+          <button onClick={this._onBoldClick.bind(this)}>Bold</button>
+          <button onClick={this._onItalicClick.bind(this)}>Italicize</button>
+          <button onClick={this._onUnderlineClick.bind(this)}>Underline</button>
+          <Editor
           editorState = {this.props.editorState || EditorState.createEmpty()}
           onChange = {this.props.onChange}
-        />
+          />
       </div>
     </div>
     );
