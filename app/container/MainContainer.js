63683636766c451f4e6663a8c8348e57d9316237
Fahim Khan <fkhan@bates.edu>
2018-08-28 10:08:46 -0700

    undid direct edits

--- app/container/MainContainer.js
+++ app/container/MainContainer.js
@@ -5,49 +5,13 @@ import { Editor, EditorState, RichUtils } from 'draft-js';
 import { onChange } from '../actions/index';
 
 const styleMap = {
-  'FONTSIZE_10': {
-    fontSize: 10,
-  },
-  'FONTSIZE_12': {
-    fontSize: 12,
-  },
-  'FONTSIZE_14': {
-    fontSize: 14,
-  },
-  'FONTCOLOR_red': {
-    color: "red"
-  },
-  'FONTCOLOR_black': {
-    color: "black"
-  },
-  'FONTCOLOR_blue': {
-    color: "blue"
-  },
-  'ALIGN_center': { //zzzz
-    textAlign: "center"
-  },
-  'ALIGN_left': {
-    float: "left"
-  },
-  'ALIGN_right': {
-    float: "right"
-  },
-};
-
-const getBlockStyle = (block) => { //zzzz
-  console.log(block.getType());
-  console.log(block);
-    switch (block.getType()) {
-        case 'left':
-            return 'align-left';
-        case 'center':
-            return 'align-center';
-        case 'right':
-            return 'align-right';
-        default:
-            return null;
-    }
-}
+   'FONTSIZE': {
+     fontSize: 10,
+   },
+   'FONTCOLOR': {
+     color: "red"
+   }
+  };
 
 class MainContainer extends React.Component {
   _onBoldClick() {
@@ -71,87 +35,33 @@ class MainContainer extends React.Component {
     ));
   }
 
-  _onFontSizeClick(e) {
-    console.log(e.target.getAttribute("value"))
-    let fontSize = e.target.getAttribute("value");
+  _onFontSizeClick() {
     this.props.onChange(RichUtils.toggleInlineStyle(
       this.props.editorState,
-      'FONTSIZE_'+fontSize,
+      'FONTSIZE'
     ));
   }
 
-  _onFontColorClick(e) {
-    let fontColor = e.target.getAttribute("value");
+  _onFontColorClick() {
     this.props.onChange(RichUtils.toggleInlineStyle(
       this.props.editorState,
-      'FONTCOLOR_'+fontColor,
-    ));
-  }
-
-  _onTextAlignClick(e) { //zzzz
-    let textAlign = e.target.getAttribute("value");
-    console.log(textAlign);
-    this.props.onChange(RichUtils.toggleInlineStyle(
-      this.props.editorState,
-      'ALIGN_'+textAlign,
-    ));
-  }
-
-  _createBulletPoints() {
-    this.props.onChange(RichUtils.toggleBlockType(
-      this.props.editorState,
-      'unordered-list-item'
-    ));
-  }
-
-  _createNumberPoints() {
-    this.props.onChange(RichUtils.toggleBlockType(
-      this.props.editorState,
-      'ordered-list-item'
+      'FONTCOLOR'
     ));
   }
 
   render() {
     return (
       <div>
-        <div className="buttons">
-          <button onClick={this._onBoldClick.bind(this)}>Bold</button>
-          <button onClick={this._onItalicClick.bind(this)}>Italicize</button>
-          <button onClick={this._onUnderlineClick.bind(this)}>Underline</button>
-          <button onClick={this._createBulletPoints.bind(this)}>Bullet points</button>
-          <button onClick={this._createNumberPoints.bind(this)}>Number points</button>
-          <div className="dropdown">
-          <button>Font Size</button>
-            <div className="dropdown-content">
-              <a href="#" onClick={this._onFontSizeClick.bind(this)} value='10'>10</a>
-              <a href="#" onClick={this._onFontSizeClick.bind(this)} value='12'>12</a>
-              <a href="#" onClick={this._onFontSizeClick.bind(this)} value='14'>14</a>
-            </div>
-          </div>
-          <div className="dropdown">
-            <button>Font Color</button>
-            <div className="dropdown-content">
-              <a href="#" onClick={this._onFontColorClick.bind(this)} value='black'>black</a>
-              <a href="#" onClick={this._onFontColorClick.bind(this)} value='red'>red</a>
-              <a href="#" onClick={this._onFontColorClick.bind(this)} value='blue'>blue</a>
-            </div>
-          </div>
-          <div className="dropdown">
-            <button>Align Text</button>
-            <div className="dropdown-content">
-              {/* //zzzz */}
-              <a href="#" onClick={this._onTextAlignClick.bind(this)} value='center'>Align Center</a>
-              <a href="#" onClick={this._onTextAlignClick.bind(this)} value='left'>Align Left</a>
-              <a href="#" onClick={this._onTextAlignClick.bind(this)} value='right'>Alignt Right</a>
-            </div>
-          </div>
-        </div>
+        <button onClick={this._onBoldClick.bind(this)}>Bold</button>
+        <button onClick={this._onItalicClick.bind(this)}>Italicize</button>
+        <button onClick={this._onUnderlineClick.bind(this)}>Underline</button>
+        <button onClick={this._onFontSizeClick.bind(this)}>Font Size</button>
+        <button onClick={this._onFontColorClick.bind(this)}>Font Color</button>
         <div className="editor">
           <Editor
           editorState = {this.props.editorState || EditorState.createEmpty()}
           onChange = {this.props.onChange}
           customStyleMap = {styleMap}
-          blockStyleFn={getBlockStyle} //zzzz
           />
       </div>
     </div>
